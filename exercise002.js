d2724c7873d25e9680c891a9a0cbf20c3a157b33
Subha <subharamkie@gmail.com>
2023-11-08 20:27:04 +0000

    Tried a different fetch returning html

--- exercise002.js
+++ exercise002.js
@@ -1,13 +1,14 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+//const jsonTypicode = "https://apilist.fun/category/grocery";
 
 const fetchData = async (apiEndPoint) => {
   try {
     const response = await fetch(apiEndPoint);
     const json = await response.json();
     console.log(json);
-  } catch {
+  } catch (error) {
     console.log(error);
   }
 };
