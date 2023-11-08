a4f8281f159813de4e5e41652c13713feb006d4d
Subha <subharamkie@gmail.com>
2023-11-08 20:11:39 +0000

    Refactor:Changed then,catch with async,await

--- exercise001.js
+++ exercise001.js
@@ -1,20 +1,21 @@
-const promise = new Promise((resolve, reject) => {
-  // Generate random number between 0 and 9
-  const randomInt = Math.floor(Math.random() * 10);
+async function getPromiseResponse() {
+  try {
+    const promise = await new Promise((resolve, reject) => {
+      // Generate random number between 0 and 9
+      const randomInt = Math.floor(Math.random() * 10);
 
-  if (randomInt % 2 === 0) {
-    // Success
-    setTimeout(() => resolve("done"), 2000);
-  } else {
-    // Failure
-    setTimeout(() => reject("error"), 2000);
-  }
-});
+      if (randomInt % 2 === 0) {
+        // Success
+        setTimeout(() => resolve("done"), 2000);
+      } else {
+        // Failure
+        setTimeout(() => reject("error"), 2000);
+      }
+    });
 
-promise
-  .then((result) => {
-    console.log(`Yay! Promise resolved with response: ${result}`);
-  })
-  .catch((error) => {
+    console.log(`Yay! Promise resolved with response: ${promise}`);
+  } catch (error) {
     console.log(`Boo. Promise rejected with response:${error} `);
-  });
+  }
+}
+getPromiseResponse();
